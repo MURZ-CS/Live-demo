1606710965e97abded3c93f495cae2823fcddf09
Muneeb ur Rehman <muneeb.ur.rehman.cs@gmail.com>
2025-11-11 09:54:16 +0500

    Create test_cal.py

---
+++ tests/test_cal.py
@@ -0,0 +1,18 @@
+from mycode.calculator import add,subtract,multiply,divide 
+import pytest
+
+def test_add():
+  assert add(2,3)==5
+
+def test_subtract():
+  assert subtract(5,2)==3
+
+def test multiply():
+  assert multiply(3,4)==12
+
+def test_divide():
+  assert divide(10,2)==5
+
+def test_zero_case():
+  with pytest.raises(ValueError):
+    divide(5,0)
